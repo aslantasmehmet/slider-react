d7e58f0076703176be5f78418339ca22ddbab958
MehmetASLANTAŞ <aslantas-mehmet58@hotmail.com>
2023-02-02 19:27:56 +0300

    The slider has been successfully designed.

--- src/api/data.js
+++ src/api/data.js
@@ -5,7 +5,7 @@ const people = [
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
       name: 'maria ferguson',
       title: 'office manager',
-      quote:
+      description:
         'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
     },
     {
@@ -14,7 +14,7 @@ const people = [
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
       name: 'john doe',
       title: 'regular guy',
-      quote:
+      description:
         'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
     },
     {
@@ -23,7 +23,7 @@ const people = [
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
       name: 'peter smith',
       title: 'product designer',
-      quote:
+      description:
         'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
     },
     {
@@ -32,7 +32,7 @@ const people = [
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
       name: 'susan andersen',
       title: 'the boss',
-      quote:
+      description:
         'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
     },
   ];
--- src/pages/Slider.js
+++ src/pages/Slider.js
@@ -1,26 +1,92 @@
-import React from "react";
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
- import {FaQuoteRight}from 'react-icons/fa';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import people from "../api/data";
 
 export default function Slider() {
-  return (
-    <div className="bg-stone-100 w-full h-screen" >
-      <div className="flex flex-row translate-y-64 ">
-        <button class="basis-1/4 flex justify-center"><FiChevronLeft className="border w-8 h-8 rounded-lg bg-slate-600 text-white  hover:bg-red-600"/></button>
-        <div class="basis-1/2 flex justify-center">
-            <div className="flex flex-col -translate-y-40">
-                <div className="border-2 text-slate-400 shadow-lg w-40 h-40 rounded-full translate-x-64"></div>
-                
-                    <p className="font-bold text-xl text-red-600 mt-4">MEHMET ASLANTAŞ</p>
-                    <p className="font-normal text-slate-400 text-base mt-2">Software Developer</p>
-                    <p className="mt-10">Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.</p>
-                   <div className="flex justify-center"> <FaQuoteRight className=" text-4xl text-red-600 mt-8"/></div>
-             
-                
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [data, setData] = useState([]);
+  const [intervalId, setIntervalId] = useState(null);
 
-            </div>
+  useEffect(() => {
+    setData(people);
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (currentIndex < data.length - 1) {
+        setCurrentIndex(currentIndex + 1);
+      } else {
+        setCurrentIndex(0);
+      }
+    }, 4000);
+    setIntervalId(interval);
+    return () => clearInterval(interval);
+  }, [currentIndex, data.length]);
+
+  const handlePrevious = () => {
+    clearInterval(intervalId);
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+  const handleNext = () => {
+    clearInterval(intervalId);
+    if (currentIndex < data.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      setCurrentIndex(0);
+    }
+  };
+  return (
+    <div className="bg-stone-100 w-full h-screen">
+      <div className="flex flex-row translate-y-64 ">
+        <button
+          class="basis-1/4 flex justify-center"
+          onClick={handlePrevious}
+          disabled={currentIndex === 0}
+        >
+          <FiChevronLeft className="border w-10 h-10 rounded-lg bg-slate-600 text-white drop-shadow-2xl hover:bg-red-600" />
+        </button>
+        <div class="basis-1/2 flex justify-center">
+          {data.length &&
+            data.map((dataPeople, index) => {
+              if (index === currentIndex) {
+                return (
+                  <div
+                    key={dataPeople.id}
+                    className="flex flex-col -translate-y-56 justify-center"
+                  >
+                    <img
+                      className="border-4 border-slate-400 text-slate-400 shadow-xl  w-32 h-32 rounded-full translate-x-64 ml-4 scale-125"
+                      src={dataPeople.image}
+                    />
+
+                    <p className="font-bold text-xl text-red-600 mt-4">
+                      {dataPeople.name}
+                    </p>
+                    <p className="font-normal text-slate-400 text-base mt-2">
+                      {dataPeople.title}
+                    </p>
+                    <p className="mt-10 text-slate-400">
+                      {dataPeople.description}
+                    </p>
+                    <div className="flex justify-center">
+                      <FaQuoteRight className=" text-4xl text-red-600 mt-8" />
+                    </div>
+                  </div>
+                );
+              }
+              return null;
+            })}
         </div>
-        <button class="basis-1/4 flex justify-center"><FiChevronRight className="border w-8 h-8 rounded-lg bg-slate-600 text-white  hover:bg-red-600"/></button>
+        <button
+          class="basis-1/4 flex justify-center"
+          onClick={handleNext}
+          disabled={currentIndex === data.length - 1}
+        >
+          <FiChevronRight className="border w-10 h-10 rounded-lg bg-slate-600 text-white  hover:bg-red-600" />
+        </button>
       </div>
     </div>
   );
